30255df8112310c920ad845a5fcee0a516c5f002
SamuelJDixon <samuel.dixon@my.jcu.edu.au>
2016-04-05 12:28:02 +1000

    Changes made to code, list better

--- Assessment 1.py
+++ Assessment 1.py
@@ -2,15 +2,18 @@
 MENU = "\nMenu:\n(L)ist all items\n(H)ire and item\n(R)eturn an item\n(A)dd a new item to stock\n(Q)uit"
 user_name = input("Please enter name: ")
 
+
+
 def main():
     print("Welcome", user_name, "to Samuel Dixon's online catalogue")
-
+    with open('items.csv') as f:
+            item_list = f.read().splitlines()
 
     print(MENU)
-    choice = input(">>> ").upper()
+    choice = input("Please choose a menu option: ").upper()
     while choice != "Q":
         if choice == "L":
-            print("L")
+            print(item_list)
         elif choice == "H":
             print("H")
         elif choice == "R":
@@ -20,8 +23,16 @@ def main():
         else:
             print("Invalid Menu Choice")
         print(MENU)
-        choice = input(">>> ").upper()
+        choice = input("Please choose a menu option: ").upper()
 
     print("Thank you for using Samuel Dixon's online catalogue")
 
+
+
+
+
+
+
 main()
+
+
