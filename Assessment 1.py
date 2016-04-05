fac971e81fde96cf314fbe0b842c694762eb6f67
SamuelJDixon <samuel.dixon@my.jcu.edu.au>
2016-04-05 14:41:02 +1000

    Heyo, updated files

--- Assessment 1.py
+++ Assessment 1.py
@@ -6,14 +6,27 @@ user_name = input("Please enter name: ")
 
 def main():
     print("Welcome", user_name, "to Samuel Dixon's online catalogue")
-    with open('items.csv') as f:
-            item_list = f.read().splitlines()
+    open_file = open('items.csv', 'r+')
+    allLines = open_file.readlines()
+
+    #Create a new string or list, outside the nested for loops. Master string that will be saved in the end.
+    #create a new integer and initialise it with zero
+
+    for line in allLines:
+        #if line index is equal to user interger.... do this
+        currentLine = line.split(',')
+        for element in currentLine:
+            print(element)
+            #add element to the string ie, if in add out
+
+        #add a newline character to end of string
+        #increment the int variable
 
     print(MENU)
     choice = input("Please choose a menu option: ").upper()
     while choice != "Q":
         if choice == "L":
-            print(item_list)
+            print(allLines)
         elif choice == "H":
             print("H")
         elif choice == "R":
@@ -26,12 +39,6 @@ def main():
         choice = input("Please choose a menu option: ").upper()
 
     print("Thank you for using Samuel Dixon's online catalogue")
-
-
-
-
-
-
 
 main()
 
