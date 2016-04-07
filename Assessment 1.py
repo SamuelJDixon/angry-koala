06883b7f92e56439c995f933f96590b7c93ebf5d
SamuelJDixon <samuel.dixon@my.jcu.edu.au>
2016-04-07 15:22:18 +1000

    Code almost done
    Need formatting, fix the CSV export. Also need to add error checking for the user inputs.

--- Assessment 1.py
+++ Assessment 1.py
@@ -1,16 +1,18 @@
 
 MENU = "\nMenu:\n(L)ist all items\n(H)ire and item\n(R)eturn an item\n(A)dd a new item to stock\n(Q)uit"
+
 user_name = input("Please enter name: ")
+import csv
 open_file = open('items.csv', 'r+')
 all_lines = []
 for line in open_file:
-            splitline = line.split(',')
-            all_lines.append((splitline[0], splitline[1], splitline[2], splitline[3]))
+    splitline = line.split(',')
+    all_lines.append((splitline[0], splitline[1], splitline[2], splitline[3]))
 
 def main():
     print("Welcome", user_name, "to Samuel Dixon's online catalogue")
-
     print(MENU)
+
     choice = input("Please choose a menu option: ").upper()
     while choice != "Q":
         if choice == "L":
@@ -25,7 +27,11 @@ def main():
             print("Invalid Menu Choice")
         print(MENU)
         choice = input("Please choose a menu option: ").upper()
-
+# Below code should work for writing the CSV, having trouble and need to look into it. Adding quotes and new lines.
+    # out_file = open('items.csv', 'w', newline='')
+    # writer = csv.writer(out_file, dialect='excel')
+    # writer.writerows(all_lines)
+    # out_file.close()
     print("Thank you for using Samuel Dixon's online catalogue")
 
 def hire():
@@ -63,49 +69,21 @@ def return_item():
 
 
 def add_item():
-    # name = input("Enter a name for the item: ")
-    # desc = input("Enter a description for the item: ")
-    # price = float(input("Enter a price for the item: "))
-    # status = "in"
-    # append_item = []
-    # append_item[0] = (all_lines[name][0], all_lines[desc][1], all_lines[price][2], all_lines[int(status)][3])
-    #     #(name[0], desc[1], str(price)[2], statu[3])
 
-    name = input('enter item name:')
-    description = input('please enter the item description')
-    price = float(input('enter the price'))
-    str_price = str(price)
-    status = "in"
-    append_item = []
-    new_item = name +','+ description +','+ str_price +','+ status
-    print(new_item)
-    append_item = [new_item]
-    all_lines.append(append_item)
+    name = input('Enter name of the item: ')
+    description = input('Please enter the item description: ')
+    while True:
+        try:
+            price = float(input("Please enter the price of the item: "))
+            break
+        except ValueError:
+            print("Sorry! That was not a valid number. Try again please...")
+    status = "in\n"
+    new_item = (name, description, price, status)
+    number_change = len(all_lines)+1
+    all_lines.insert(number_change, new_item)
+    print("\nYou have added the item: ", new_item)
 
 main()
-
-
-
-
-
-
-
-
-    #Create a new string or list, outside the nested for loops. Master string that will be saved in the end.
-    # final_string = ""
-    # counter = 0
-    # str_counter = str(counter)
-    # for line in all_lines: #iterates through each lines
-    #         str_counter = str(counter) #converts counter to a string
-    #         current_line = line.split(',')
-    #         final_string += str_counter #adds integer to beginning of line
-    #         final_string += ': '
-    #         for element in current_line:  #iterates through each element inside the current line
-    #             final_string += element #add element to the string
-    #             if element == 'out\n' or element == 'in\n': #tests if it is the last element
-    #                 final_string += '\n' #add a new line after each item status
-    #             else:
-    #                 final_string += ', '
-    #         counter += 1
 
 
